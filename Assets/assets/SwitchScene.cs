7550a12a8443be75b1436761ccc9f9632d9ffaa0
cockyfour <hughesty66@gmail.com>
2024-02-11 23:16:31 -0800

    final

---
+++ Assets/assets/SwitchScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        SceneManager.LoadScene(1);
+
+
+    }
+    
+   
+}
